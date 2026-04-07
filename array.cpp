1af606a91ecb488bce1afe12edc9e5153af83eb7
fathannnur <carelius02@gmail.com>
2026-04-07 17:19:15 +0700

    Menambahkan deklarasi

--- array.cpp
+++ array.cpp
@@ -3,3 +3,7 @@ using namespace std;
 
 int main() 
 {
+    // deklarasi array di sertai dengan inisialisasi
+    int dataku[5] = {10, 20, 30, 40, 50};
+    //deklarasi array tanpa inisialisasi
+    int datamu[5];
