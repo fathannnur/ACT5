b08fda9d99968d5ea63e70283c745674de0ad6b8
fathannnur <carelius02@gmail.com>
2026-04-07 17:20:52 +0700

    Menambahkan cara akses

--- array.cpp
+++ array.cpp
@@ -7,3 +7,9 @@ int main()
     int dataku[5] = {10, 20, 30, 40, 50};
     //deklarasi array tanpa inisialisasi
     int datamu[5];
+
+    //cara akses (membaca atau mengabil isi array
+    cout << "Isi array data kedua: " << dataku[1] << endl;
+    //mengisi array
+    dataku [1] = 200;
+    cout << "Isi array data kedua setelah diubah: " << dataku[1] << endl;
