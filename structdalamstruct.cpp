e579d73728aca344587548cbf8fa9e41214b7a4d
fathannnur <carelius02@gmail.com>
2026-04-07 18:44:51 +0700

    Menambahkan cout

--- structdalamstruct.cpp
+++ structdalamstruct.cpp
@@ -14,3 +14,25 @@ struct orang
     detailAlamat alamat;
     int umur;
 };
+
+int main()
+{
+    // deklarasi variable struct
+    orang mhs;
+    // mengisi data
+    cout << "Isikan data nama : ";
+    cin >> mhs.nama;
+    cout << "Isikan data desa : ";
+    cin >> mhs.alamat.desa;
+    cout << "Isikan data kota : ";
+    cin >> mhs.alamat.kota;
+    cout << "Isikan data umur : ";
+    cin >> mhs.umur;
+    cout << endl;
+
+    cout <<"Data Orang" << endl;
+    cout << "Nama : " << mhs.nama << endl;
+    cout << "Desa : " << mhs.alamat.desa << endl;
+    cout << "Kota : " << mhs.alamat.kota << endl;
+    cout << "Umur : " << mhs.umur << endl;
+}
